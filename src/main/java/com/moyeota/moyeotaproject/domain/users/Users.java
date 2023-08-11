4666b3d07035569446ee740011a0a482dce9802c
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-08-11 22:38:31 +0900

    feat: builder 패턴

--- src/main/java/com/moyeota/moyeotaproject/domain/users/Users.java
+++ src/main/java/com/moyeota/moyeotaproject/domain/users/Users.java
@@ -2,6 +2,7 @@ package com.moyeota.moyeotaproject.domain.users;
 
 
 import com.moyeota.moyeotaproject.domain.posts.Posts;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
@@ -36,16 +37,16 @@ public class Users {
     private String status;
 
     @Column(nullable = false)
-    private boolean gender;
+    private Boolean gender;
 
     @Column(nullable = false)
-    private float averageStarRate;
+    private Float averageStarRate;
 
     @Column(nullable = false)
     private String school;
 
     @Column(nullable = false)
-    private boolean isAuthenticated;
+    private Boolean isAuthenticated;
 
     @OneToMany(mappedBy = "user")
     private List<Posts> posts = new ArrayList<>();
@@ -57,6 +58,17 @@ public class Users {
     public void addPost(Posts post) {
         posts.add(post);
         post.setUser(this);
+    }
+
+    @Builder
+    public Users(String name, String profileImage, String password, Boolean gender, Float averageStarRate, String school, Boolean isAuthenticated) {
+        this.name = name;
+        this.profileImage = profileImage;
+        this.password = password;
+        this.gender = gender;
+        this.averageStarRate = averageStarRate;
+        this.school = school;
+        this.isAuthenticated = isAuthenticated;
     }
 
 }
