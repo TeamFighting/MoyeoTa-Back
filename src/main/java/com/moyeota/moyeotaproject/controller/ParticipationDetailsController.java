1b546a49038aa56416e3e5183e0929ca0d183d40
rhrudska987 <rhrudska987@naver.com>
2023-08-26 19:14:22 +0900

    chore: uri 변경

--- src/main/java/com/moyeota/moyeotaproject/controller/ParticipationDetailsController.java
+++ src/main/java/com/moyeota/moyeotaproject/controller/ParticipationDetailsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/participationDetails")
+@RequestMapping("/api/participation-details")
 public class ParticipationDetailsController {
 
     private final ParticipationDetailsService participationDetailsService;
