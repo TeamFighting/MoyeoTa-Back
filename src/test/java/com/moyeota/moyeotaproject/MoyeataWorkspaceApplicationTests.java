37931561ace3daadae09cb06e2a6383a4ab847f1
rhrudska987 <rhrudska987@naver.com>
2023-09-02 19:46:13 +0900

    fix: 주석처리

--- src/test/java/com/moyeota/moyeotaproject/MoyeataWorkspaceApplicationTests.java
+++ src/test/java/com/moyeota/moyeotaproject/MoyeataWorkspaceApplicationTests.java
@@ -3,7 +3,7 @@ package com.moyeota.moyeotaproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MoyeataWorkspaceApplicationTests {
 
     @Test
