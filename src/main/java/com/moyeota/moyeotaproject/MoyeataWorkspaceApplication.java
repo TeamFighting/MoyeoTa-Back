b4b78580154a2387b5c58f4b7053130a3f2c5b79
username <57055730+Jake-huen@users.noreply.github.com>
2023-11-01 16:24:35 +0900

    feat : ConfigurationPropertiesScan 추가

--- src/main/java/com/moyeota/moyeotaproject/MoyeataWorkspaceApplication.java
+++ src/main/java/com/moyeota/moyeotaproject/MoyeataWorkspaceApplication.java
@@ -3,6 +3,7 @@ package com.moyeota.moyeotaproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
 })
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class MoyeataWorkspaceApplication {
 
     public static void main(String[] args) {
