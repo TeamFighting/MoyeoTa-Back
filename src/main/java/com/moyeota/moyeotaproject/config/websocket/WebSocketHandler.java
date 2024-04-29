cefba6b0a66748f40b44e101ede5b953d1c6938f
고경남 <rhrudska987@naver.com>
2024-04-29 20:34:38 +0900

    feat: 경로 찾기 API 추가

---
+++ src/main/java/com/moyeota/moyeotaproject/config/websocket/WebSocketHandler.java
@@ -0,0 +1,46 @@
+package com.moyeota.moyeotaproject.config.websocket;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.moyeota.moyeotaproject.domain.location.Location;
+import com.moyeota.moyeotaproject.domain.location.LocationRepository;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+	private final LocationRepository locationRepository;
+	private static final ConcurrentHashMap<String, WebSocketSession> CLIENTS = new ConcurrentHashMap<String, WebSocketSession>();
+
+	public WebSocketHandler(LocationRepository locationRepository) {
+		this.locationRepository = locationRepository;
+	}
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		CLIENTS.put(session.getId(), session);
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		CLIENTS.remove(session.getId());
+	}
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		String id = session.getId();  //메시지를 보낸 아이디
+		Location location = Location.builder()
+			.sessionId(id)
+			.position(message.getPayload())
+			.build();
+		locationRepository.save(location);
+	}
+
+}
--- src/main/java/com/moyeota/moyeotaproject/service/WebSocketChatHandler/WebSocketConfig.java
+++ src/main/java/com/moyeota/moyeotaproject/service/WebSocketChatHandler/WebSocketConfig.java
@@ -2,22 +2,38 @@ package com.moyeota.moyeotaproject.service.WebSocketChatHandler;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws", "wss").setAllowedOriginPatterns("*").withSockJS();
-    }
+import lombok.RequiredArgsConstructor;
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/chatroom","/user");
-        registry.setUserDestinationPrefix("/user");
-    }
+@Configuration
+@EnableWebSocket
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSocketConfigurer {
+
+	private final WebSocketHandler webSocketHandler;
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(webSocketHandler, "/tracking").setAllowedOrigins("*");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws", "wss").setAllowedOriginPatterns("*").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+		registry.enableSimpleBroker("/chatroom", "/user");
+		registry.setUserDestinationPrefix("/user");
+	}
 }
