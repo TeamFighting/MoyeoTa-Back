322fc5f4327e151c6e1d38dcce38a438bc013008
username <57055730+Jake-huen@users.noreply.github.com>
2024-04-30 20:30:04 +0900

    FIX : 페이지 오류 해결

--- src/main/java/com/moyeota/moyeotaproject/service/SchoolService.java
+++ src/main/java/com/moyeota/moyeotaproject/service/SchoolService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moyeota.moyeotaproject.dto.UsersDto.SchoolDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,14 +18,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SchoolService {
 
+    @Value("${school.api.key}")
+    private String apiKey;
+
     private final String schoolApiUrl = "http://www.career.go.kr/cnet/openapi/getOpenApi.xml";
-    private final String apiKey = "15495ecea92f26bae8712b88f11e02e3";
     private final RestTemplate restTemplate;
 
     public List<SchoolDto.schoolInfo> searchSchool() {
         List<SchoolDto.schoolInfo> result = new ArrayList<>();
         for (int page = 1; page < 25; page++) {
-            String requestUrl = String.format("%s?apiKey=%s&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&page=%s", schoolApiUrl, apiKey, page);
+            String requestUrl = String.format("%s?apiKey=%s&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&thisPage=%s", schoolApiUrl, apiKey, page);
             log.info("requestUrl = {}", requestUrl);
             SchoolJson schoolJson = restTemplate.getForObject(requestUrl, SchoolJson.class);
             schoolJson.getDataSearch().getContent().forEach(school -> {
