60ff438650c6898ae944a25dd31b1729ba9cbec6
Jake-huen <tae77777@naver.com>
2024-05-08 15:05:28 +0900

    FEAT : 계좌 추가 및 조회 테스트 코드 작성

--- src/main/java/com/moyeota/moyeotaproject/dto/UsersDto/AccountDto.java
+++ src/main/java/com/moyeota/moyeotaproject/dto/UsersDto/AccountDto.java
@@ -1,10 +1,13 @@
 package com.moyeota.moyeotaproject.dto.UsersDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class AccountDto {
 
     private String bankName;
--- src/test/java/com/moyeota/moyeotaproject/service/UsersServiceTest.java
+++ src/test/java/com/moyeota/moyeotaproject/service/UsersServiceTest.java
@@ -2,8 +2,11 @@ package com.moyeota.moyeotaproject.service;
 
 import com.moyeota.moyeotaproject.config.jwtconfig.JwtTokenGenerator;
 import com.moyeota.moyeotaproject.config.jwtconfig.JwtTokenProvider;
+import com.moyeota.moyeotaproject.domain.account.Account;
+import com.moyeota.moyeotaproject.domain.account.AccountRepository;
 import com.moyeota.moyeotaproject.domain.users.Users;
 import com.moyeota.moyeotaproject.domain.users.UsersRepository;
+import com.moyeota.moyeotaproject.dto.UsersDto.AccountDto;
 import com.moyeota.moyeotaproject.dto.UsersDto.TokenInfoDto;
 import com.moyeota.moyeotaproject.dto.UsersDto.UserDto;
 import com.moyeota.moyeotaproject.dto.UsersDto.UsersResponseDto;
@@ -17,10 +20,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @Slf4j
@@ -32,37 +39,21 @@ class UsersServiceTest {
     @MockBean
     private UsersRepository usersRepository;
 
+    @MockBean
+    private AccountRepository accountRepository;
+
     @Autowired
     private UsersService usersService;
 
-    private String accessToken;
+    @Autowired
+    private JwtTokenGenerator jwtTokenGenerator;
 
-    String salt = "RlaXoVBsYt9V7zq57TejMnVUyzblYcfPQye08f7MGVA9XkHa";
-
-    public JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(salt);
-    public JwtTokenGenerator jwtTokenGenerator = new JwtTokenGenerator(jwtTokenProvider);
+    private Users userEntity;
 
     @BeforeEach
     void beforeEach() {
-        Long userId = 1L;
-        TokenInfoDto generate = jwtTokenGenerator.generate(userId);
-        accessToken = "Bearer " + generate.getAccessToken();
-
-        // 테스트를 위한 가상의 사용자 저장
-        Users userEntity = Users.builder()
-                .id(userId)
-                .name("테스트사용자")
-                .email("tae77777@naver.com")
-                .age("24")
-                .loginId("moyeota")
-                .password("moyeota")
-                .build();
-        when(usersRepository.findById(userId)).thenReturn(Optional.of(userEntity));
-    }
-
-    @BeforeEach
-    public void 유저_저장() {
-        Users users = Users.builder()
+        userEntity = Users.builder()
+                .id(1L)
                 .name("테스트사용자")
                 .profileImage("https://ssl.pstatic.net/static/pwe/address/img_profile.png")
                 .phoneNumber("010-7757-6340")
@@ -76,58 +67,108 @@ class UsersServiceTest {
                 .isAuthenticated(false)
                 .age("24")
                 .build();
-        usersRepository.save(users);
-        System.out.println("users.getId() = " + users.getId());
+        when(usersRepository.findById(userEntity.getId())).thenReturn(Optional.ofNullable(userEntity));
+        when(usersRepository.save(userEntity)).thenReturn(userEntity);
     }
 
-//    @Test
-//    public void 토큰으로_유저_조회_테스트() {
-//        // given
-//        // when
-//        Users userByToken = usersService.getUserByToken(accessToken);
-//
-//        // then
-//        assertThat(userByToken.getId()).isEqualTo(1L);
-//    }
-//
-//    @Test
-//    public void 유저_정보_조회() {
-//        // when
-//        UserDto.Response users = usersService.getInfo(accessToken);
-//
-//        // then
-//        assertThat(users.getName()).isEqualTo("테스트사용자");
-//        assertThat(users.getLoginId()).isEqualTo("moyeota");
-//        assertThat(users.getGender()).isEqualTo("MALE");
-//        assertThat(users.getSchool()).isEqualTo("건국대학교");
-//        assertThat(users.getAge()).isEqualTo("24");
-//    }
-//
-//    @Test
-//    @Order(3)
-//    public void 유저_닉네임_생성() {
-//        // given
-//
-//        // when
-//        UsersResponseDto usersWithNickName = usersService.createNickName(accessToken, "첫 닉네임");
-//
-//        // then
-//        assertThat(usersWithNickName.getNickName()).isEqualTo("첫 닉네임");
-//    }
-//
-//
-//    @Test
-//    @Order(4)
-//    public void 유저_닉네임_수정() {
-//        // given
-//        String newNickName = "모여타중독자";
-//
-//        // when
-//        UsersResponseDto usersResponseDto = usersService.updateNickName(accessToken, newNickName);
-//
-//        // then
-//        assertThat(usersResponseDto.getNickName()).isEqualTo("모여타중독자");
-//    }
+    @Test
+    public void 토큰으로_유저_조회_테스트() {
+        // given
+        TokenInfoDto generate = jwtTokenGenerator.generate(userEntity.getId());
+        String accessToken = "Bearer " + generate.getAccessToken();
+
+        // when
+        Users userByToken = usersService.getUserByToken(accessToken);
+
+        // then
+        assertThat(userByToken.getId()).isEqualTo(1L);
+        assertThat(userByToken.getEmail()).isEqualTo("tae77777@naver.com");
+    }
+
+    @Test
+    public void 유저_정보_조회() {
+        // given
+        TokenInfoDto generate = jwtTokenGenerator.generate(userEntity.getId());
+        String accessToken = "Bearer " + generate.getAccessToken();
+
+        // when
+        UserDto.Response users = usersService.getInfo(accessToken);
+
+        // then
+        assertThat(users.getName()).isEqualTo("테스트사용자");
+        assertThat(users.getProfileImage()).isEqualTo("https://ssl.pstatic.net/static/pwe/address/img_profile.png");
+        assertThat(users.getPhoneNumber()).isEqualTo("010-7757-6340");
+        assertThat(users.getEmail()).isEqualTo("tae77777@naver.com");
+        assertThat(users.getLoginId()).isEqualTo("moyeota");
+        assertThat(users.getStatus()).isEqualTo("GOOGLE");
+        assertThat(users.getGender()).isEqualTo("MALE");
+        assertThat(users.getAverageStarRate()).isEqualTo(3.2f);
+        assertThat(users.getSchool()).isEqualTo("건국대학교");
+        assertThat(users.getAge()).isEqualTo("24");
+    }
+
+    @Test
+    public void 유저_닉네임_생성() {
+        // given
+        TokenInfoDto generate = jwtTokenGenerator.generate(userEntity.getId());
+        String accessToken = "Bearer " + generate.getAccessToken();
+
+        // when
+        UsersResponseDto usersWithNickName = usersService.createNickName(accessToken, "첫 닉네임");
+
+        // then
+        assertThat(usersWithNickName.getNickName()).isEqualTo("첫 닉네임");
+    }
+
+
+    @Test
+    public void 유저_닉네임_수정() {
+        // given
+        TokenInfoDto generate = jwtTokenGenerator.generate(userEntity.getId());
+        String accessToken = "Bearer " + generate.getAccessToken();
+        String newNickName = "모여타중독자";
+
+        // when
+        UsersResponseDto usersResponseDto = usersService.updateNickName(accessToken, newNickName);
+
+        // then
+        assertThat(usersResponseDto.getNickName()).isEqualTo("모여타중독자");
+    }
+
+    @Test
+    public void 유저_계좌_추가() {
+        // given
+        TokenInfoDto generate = jwtTokenGenerator.generate(userEntity.getId());
+        String accessToken = "Bearer " + generate.getAccessToken();
+        String bankName = "토스";
+        String accountNumber = "1000-900231415";
+
+        // when
+        when(accountRepository.save(any())).thenReturn(Account.builder().bankName(bankName).accountNumber(accountNumber).build());
+        UserDto.AccountResponse accountResponse = usersService.addAccount(accessToken, new AccountDto(bankName, accountNumber));
+
+        // then
+        assertThat(accountResponse.getAccountDtoList()).extracting("bankName").contains(bankName);
+        assertThat(accountResponse.getAccountDtoList()).extracting("accountNumber").contains(accountNumber);
+    }
+
+    @Test
+    public void 계좌_추가_후_유저_조회시_계좌_조회() {
+        // given
+        TokenInfoDto generate = jwtTokenGenerator.generate(userEntity.getId());
+        String accessToken = "Bearer " + generate.getAccessToken();
+        String bankName = "토스";
+        String accountNumber = "1000-900231415";
+
+        // when
+        when(accountRepository.save(any())).thenReturn(Account.builder().bankName(bankName).accountNumber(accountNumber).build());
+        UserDto.AccountResponse accountResponse = usersService.addAccount(accessToken, new AccountDto(bankName, accountNumber));
+        List<AccountDto> accountDtoList = usersService.getInfo(accessToken).getAccountDtoList();
+
+        // then
+        assertThat(accountDtoList).extracting("bankName").contains(bankName);
+        assertThat(accountDtoList).extracting("accountNumber").contains(accountNumber);
+    }
 
 
 }
